57266073b632eb2cb7e91fb07499d0061f495b23
VIVEK KUMAR <121549772+Vivekkumar121@users.noreply.github.com>
2023-03-13 10:39:53 +0530

    Add files via upload

---
+++ C program/evenodd.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+int main()
+{
+    int b,n,m;
+    printf("enter any number:--");
+    scanf("%d",&b);
+    /*n=b%2;*/
+    if(b%2==0){
+        printf("the number is evenn");
+        }
+    else 
+    printf("the number is odd");
+    return 0;
+}
