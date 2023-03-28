eae336c46e067753fbcfd22375ee5f5c0e40e2d4
VIVEK KUMAR <121549772+Vivekkumar121@users.noreply.github.com>
2023-03-28 09:35:25 +0530

    Add files via upload

---
+++ Assignment_1/six.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include <string.h>
+int main(){
+    char a[100]="vivek kumar is a good boy";
+    char b[20]="okay i know that";
+    char c[100]="okadasd";
+    char d[100]="quwet";
+        // printf("enter the characters of a string");
+    // scanf("%[^\n]s",&a);
+    // printf("the string is %s\n",a);
+
+    // printf("the length of string a is %d\n",strlen(a));
+    // printf("the length of string a is %s\n",strrev(a));
+    // printf("%s\n",a);
+    // strcpy(a,b);
+    // strcat(a,b);
+    // //printf("%s\n",strcpy(c,b));
+    // printf("%s\n",a);
+    //printf("%s\n",strcat(a,b));
+   // printf("%s",strtok(a,"o"));
+    printf("%d ",strcmp(d,c));
+    
+    return 0;
+}
